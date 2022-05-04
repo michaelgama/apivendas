9b79bc46bf4e4d985c819c52173cd0cb9743bedd
Michael Gama <michael.gama@hotmail.com>
2022-05-04 00:34:42 -0300

    criacao de midware de seguranca

--- src/infra/http/middleware/rateLimiter.ts
+++ src/infra/http/middleware/rateLimiter.ts
@@ -3,31 +3,32 @@ import { NextFunction, Request, Response } from 'express';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import { createClient } from 'redis';
 
-const redisClient = createClient({
-  legacyMode: true,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-  },
-});
-
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'rateLimiter',
-  points: 3,
-  duration: 1,
-});
-
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction,
 ): Promise<void> {
+  const redisClient = createClient({
+    legacyMode: true,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+    },
+  });
+
+  await redisClient.connect();
+
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: 'rateLimiter',
+    points: 5,
+    duration: 5,
+  });
   try {
     await limiter.consume(request.ip);
 
     return next();
   } catch (err) {
-    throw new AppError('Too many requests', 429);
+    throw new AppError('Too many request', 429);
   }
 }
